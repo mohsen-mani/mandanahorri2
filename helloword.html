7b580c3f62cbf51a53ba15ee76edb6fb546ee713
mohsen-mani <122026017+mohsen-mani@users.noreply.github.com>
2023-01-05 16:26:44 +0330

    Add files via upload

---
+++ helloword.html
@@ -0,0 +1,4 @@
+alert("hello word");
+
+document.write('<h1>Mandanahorri</h1');
+prompt("Enter your name:")
---
+++ lib2.js
@@ -0,0 +1,50 @@
+ 
+var timer = 0;
+var timerInterval;
+ 
+var ms = document.getElementById('ms');
+var second = document.getElementById('second');
+var minute = document.getElementById('minute');
+
+function start() {
+  stop();
+ 
+  timerInterval = setInterval(function() {
+ 
+    timer += 1/60;
+ 
+    msVal = Math.floor((timer - Math.floor(timer))*100);
+ 
+    secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+ 
+    minuteVal = Math.floor(timer/60);
+
+
+    
+ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
+
+
+second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
+
+
+minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
+
+  }, 1000/60);
+}
+
+
+function stop() {
+  clearInterval(timerInterval);
+}
+
+function over(){
+    ms.innerHTML = 00
+
+
+    second.innerHTML = 00
+    
+    
+    minute.innerHTML = 00
+    
+    document. location. reload()
+}
